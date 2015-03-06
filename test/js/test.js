9484347d95aadfc59eacb9d6996f161556de81f9
Benjamin Gröner <bgroener@Benjis-MacBook.local>
2015-03-06 11:22:50 +0100

    [TASK]Changed the testApp to test the refactored b_dropdown

--- test/js/test.js
+++ test/js/test.js
@@ -18,24 +18,11 @@ B = B != null ? B : {};
 window.B = B;
 
 require(['jquery', 'b_dropdown'], function($, Dropdown) {
-  var initBaseDropdownWithDynamicHeader, initBaseDropdownWithStaticHeader, initSelectBasedDropdown, initialize;
+  var initBasedDropdown, initialize;
   initialize = function() {
-    initBaseDropdownWithStaticHeader();
-    initBaseDropdownWithDynamicHeader();
-    return initSelectBasedDropdown();
+    return initBasedDropdown();
   };
-  initBaseDropdownWithStaticHeader = function() {
-    return B.baseDropdownStaticHeader = new Dropdown($('#baseDropdown-staticHeader'), {
-      staticHeader: "Base Dropdown"
-    });
-  };
-  initBaseDropdownWithDynamicHeader = function() {
-    return B.baseDropdownDynamicHeader = new Dropdown($('#baseDropdown-dynamicHeader'), {
-      name: "overrodeTheOldName",
-      placeholder: "Placeholder Text"
-    });
-  };
-  initSelectBasedDropdown = function() {
+  initBasedDropdown = function() {
     return B.selectBasedDropdown = new Dropdown($('#selectDropdown'), {
       placeholder: "Please select"
     });
